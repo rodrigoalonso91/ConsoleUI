fd364fe845941c1b92c9ea8c09f76e7c64b298c1
Rodrigo Alonso <rodrigo.alonso@urbetrack.com>
2022-10-03 17:52:14 -0300

    Add project files.

---
+++ ConsoleUI/ConsoleUI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    public static class ConsoleUI
+    {
+        public static void WriteError(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"##[error] {text}");
+            Console.ResetColor();
+        }
+
+        public static void WriteInfo(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"##[info] {text}");
+            Console.ResetColor();
+        }
+
+        public static void WriteWarning(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"##[warning] {text}");
+            Console.ResetColor();
+        }
+
+        public static void WriteCommand(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"##[command] {text}");
+            Console.ResetColor();
+        }
+
+        public static void WriteOk(string text)
+        {
+            Console.WriteLine($"##[ok] {text}");
+        }
+
+        public static void WriteTitle(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"========== {text} ==========");
+            Console.ResetColor();
+        }
+    }
+}
